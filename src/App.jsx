b3e520a1c4cdd01ebfd7ee7fa32eac7230662c3d
Vivek maurya <vivek563maurya@gmail.com>
2023-09-27 01:47:59 +0530

    "setting up project file"

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1>vivek maurya</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/conf/conf.js
@@ -0,0 +1,11 @@
+const conf = {
+    appwriteUrl:String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId:String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId:String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId:String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBuketId:String(import.meta.env.VITE_APPWRITE_BUKET_ID),
+
+}
+
+
+export default conf
